dabdec0ffa40729ed540d31ab4098962bd7fa237
TimLavers <tim@grandtestauto.org>
2016-04-01 19:54:13 +1100

    Further setup.

--- src/main/java/howtoprogram/anagrams/AnagramFinder.java
+++ src/main/java/howtoprogram/anagrams/AnagramFinder.java
@@ -1,11 +1,6 @@
 package howtoprogram.anagrams;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 /**
  * Finds the anagrams of a word.
@@ -13,21 +8,14 @@ import java.util.TreeSet;
 public class AnagramFinder {
 
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("src/main/resources/books/english.txt");
-        FileBasedDictionary dictionary = new FileBasedDictionary(path);
-        String str = "battle";
-        if (!dictionary.contains(str)) {
-            System.out.println("Not in dictionary: " + str);
-        }
-        Word word = new Word(str);
-        Set<Word> anagrams = word.permutations();
-        SortedSet<String> found = new TreeSet<>();
-        for (Word w : anagrams) {
-            if (dictionary.contains(w.text())) {
-                found.add(w.text());
-            }
-        }
-        System.out.println("found = " + found);
+        //Create a dictionary from the "english.txt" file in resources/books
 
+        //Create a Word from our initial English word.
+
+        //Get all rearrangements of the letters in this word.
+
+        //For each of these, check it using the dictionary.
+
+        //If a rearrangement is in the dictionary, print it out.
     }
 }
