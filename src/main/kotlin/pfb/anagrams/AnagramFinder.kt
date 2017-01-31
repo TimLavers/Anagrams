ee2236c2341a855df5b81d8a21741e5b6c2d4751
TimLavers <tim@grandtestauto.org>
2017-01-31 13:21:46 +1100

    Better comments.

--- src/main/kotlin/pfb/anagrams/AnagramFinder.kt
+++ src/main/kotlin/pfb/anagrams/AnagramFinder.kt
@@ -6,13 +6,15 @@ import java.nio.file.Paths
  * Finds and prints the anagrams of a word.
  */
 fun main(args: Array<String>) {
-    //Create a dictionary from the "english.txt" file in resources/books
+    //Create a dictionary from "english.txt" in resources/books
 
     //Create a Term from our initial English word.
 
     //Get all rearrangements of the letters in this word.
 
-    //For each of these, check it using the dictionary.
-
-    //If a rearrangement is in the dictionary, print it out.
+    //For each of these, 
+    
+    //...if it is in the dictionary
+    
+    //...print it.
 }
